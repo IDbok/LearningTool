faa696e307bfe6aec4cf396a1440d4cc94833722
IDbok <bokarev.fic@gmail.com>
2023-10-22 22:23:51 +0400

    Завершил задачу

--- ConsoleApp7/Tasks/Task_3/Cart.cs
+++ ConsoleApp7/Tasks/Task_3/Cart.cs
@@ -6,19 +6,48 @@ using System.Threading.Tasks;
 
 namespace LearningTool.Tasks.Task_3
 {
-    internal class Cart : Shop
+    public class Cart
     {
-
-        public static void ProductCatalod(string[] productList, double[] priceList, string[] itemList)
+        string[] productCartList = new string[100];
+        double[] priceCartList = new double[100];
+        int[] amountCartList = new int[100];
+        double[] totalPriceCartList = new double[100];
+        double tatalPriceCart = 0;
+        int cartItem = 0;
+        
+        public Cart() 
         {
-            Console.WriteLine("Каталог товаров:");
-            Console.WriteLine();
-            Console.WriteLine($"Наименование:" + "     " + "Стоимость:");
-            Console.WriteLine();
-            for (int i = 0; i < productList.Length; i++)
+
+        }
+        
+        public void AddPtoduct(string product, double price, int count)
+        {
+            // TODO - проверить наличие в карзины продукта
+
+            productCartList[cartItem] = product;
+            priceCartList[cartItem] = price;
+            amountCartList[cartItem] = count;
+            totalPriceCartList[cartItem] = price * count;
+            tatalPriceCart += totalPriceCartList[cartItem];
+            cartItem++;
+        }
+        public string ShowLastItem()
+        {
+            return productCartList[cartItem - 1] + " " + amountCartList[cartItem - 1];
+        }
+        public void Show()
+        {
+            Console.WriteLine("Состав корзины:");
+            Console.WriteLine("Товар           Стоимость   Кол-во  Сумма");
+            for (int i = 0; i < productCartList.Length; i++)
             {
-                Console.WriteLine($"{productList[i],-16} {priceList[i],-4} {itemList[i],-5}");
+                if (priceCartList[i] == 0)
+                {
+                    break;
+                }
+                Console.WriteLine($"{productCartList[i],-16} {priceCartList[i],-12} {amountCartList[i],-9} {totalPriceCartList[i],-5}");
             }
+            Console.WriteLine($"Всего: {tatalPriceCart,37}");
         }
     }
 
--- ConsoleApp7/Tasks/Task_3/Shop.cs
+++ ConsoleApp7/Tasks/Task_3/Shop.cs
@@ -9,21 +9,90 @@ namespace LearningTool.Tasks.Task_3
     internal class Shop
     {
 
-        static string[] productList = { "Молоко", "Сыр", "Йогурт", "Масло", "Айран" };
-        static double[] priceList = { 100, 400, 80, 160, 100 };
-        static string[] itemList = { "руб/шт", "руб/шт", "руб/шт", "руб/шт", "руб/шт" };
+        string[] productList = { "Молоко", "Сыр", "Йогурт", "Масло", "Айран" };
+        double[] priceList = { 100, 400, 80, 160, 100 };
+        string endWord = "end";
+        bool newCart = true;
 
-        //public Shop(string[] productList, double[] priceList, string[] itemList) 
-        //{
-        //    this.productList = productList;// = { "Молоко", "Сыр", "Йогурт", "Масло", "Айран" };
-        //    this.priceList = priceList;// = { 100, 400, 80, 160, 100 };
-        //    this.itemList = itemList;// = { "руб/шт", "руб/шт", "руб/шт", "руб/шт", "руб/шт" };
+        public Shop(string[] productList, double[] priceList, string endWord)
+        {
+            this.productList = productList;
+            this.priceList = priceList;
+            this.endWord = endWord;
 
-        //}
-
-        public void AddToCart()
-        { 
+            ContactWithClient();
+        }
+        void ContactWithClient() 
+        {
+            Console.WriteLine("Добро пожаловать в наш магазин!");
+            ShowProductCatalod();
+            Console.WriteLine();
             
+            WorkWithCart();
+        }
+
+        void ShowProductCatalod()
+        {
+            Console.WriteLine("Каталог товаров:");
+            Console.WriteLine();
+            Console.WriteLine("Номер Наименование:     Стоимость:");
+            Console.WriteLine();
+            for (int i = 0; i < productList.Length; i++)
+            {
+                Console.WriteLine($"{i,5} {productList[i],-17} {priceList[i],-4} {"руб/шт",-5}");
+            }
+        }
+
+        void WorkWithCart()
+        {
+            Cart cart = new Cart();
+
+            string input;
+            do
+            {
+                Console.WriteLine($"Введите номер и ко-во товара через пробел (введите \"{endWord}\" чтобы закончить):");
+                input = Console.ReadLine();
+
+                int[] inputList = ParseInput(input);
+                // Проверка удачного парсинга ввода
+                bool goodParse = inputList[2] == 1;
+                // Проверка наличия введённого элемента в списке продуктов
+                bool isInProduct = productList.Length > inputList[0];
+                
+                if (goodParse && isInProduct)
+                {
+                    // TODO - не понял,как реализовать создание нового объекта класса
+                    // Cart при условии newCart или выполнение действий над этим классом.
+                    // Выдаёт ошибку, не существует в данном контексте
+                    
+                    cart.AddPtoduct(productList[inputList[0]], priceList[inputList[0]], inputList[1]);
+                    Console.WriteLine("Был добавлен: " + cart.ShowLastItem());
+                    
+                }
+                else if (input != endWord) { Console.WriteLine("Некорректный ввод, повторите попытку"); }
+
+            } while (input != endWord);
+            cart.Show();
+        }
+
+        static int[] ParseInput(string input)
+        {
+
+            string[] parse = input.Split(' ');
+            int[] output = { 0, 0, -1 };
+            //bool parseOK = false;
+            if (parse.Length == 2)
+            {
+
+                bool firstTry = int.TryParse(parse[0], out output[0]);
+                bool secondTry = int.TryParse(parse[1], out output[1]);
+
+                if (firstTry && secondTry)
+                {
+                    output[2] = 1;
+                }
+            }
+            return output;
         }
     }
 }
